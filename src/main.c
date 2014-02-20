066a0bf3d4a751d60235828d2d9ecb8babb4650c
Alex <acamukhin@gmail.com>
2014-02-20 19:21:04 +0700

    Прототип

--- src/main.c
+++ src/main.c
@@ -2,7 +2,25 @@
 
 int main(int argc, char *argv[], char *env[])
 {
-  printf("Hello World! My name is Alex\n");
+  char *str;
+  char id;
+  if (argc == 1) {
+  	return -1;
+  }
+  str = argv[1];
+  if (*(str) == '-') {
+  	id = *(str+1);
+  	switch (id) {
+  		case 'a':
+  			printf("YES\n");
+  			break;
+  		default:
+  			printf("NO\n");
+  			break;
+  	}
+  } else {
+  	printf("NO\n");
+  }
   return 0;
 }
 
