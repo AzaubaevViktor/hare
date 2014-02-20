2e997714e69743fd6c2be0334a2f58524cad2ac2
Korovin Viktor <sarvash.inc@gmail.com>
2014-02-20 13:10:10 +0700

    + Добавлены типы событий + Добавлена запись в файл

--- src/main.c
+++ src/main.c
@@ -4,7 +4,9 @@
 
 #define GLOBAL_LOGGING \
   char __logging_file_name[80] = ""; \
-  int __logging = 1;
+  FILE *__logging_file = NULL;
+  int __logging = 1; \
+  char __logging_buf[1000] = "";
 
 #define INIT_LOGGING \
   time_t __logging_seconds = time(NULL); \
@@ -12,14 +14,37 @@
   if (0 == strftime(__logging_file_name,80,"%d%m%Y_%H%M%S.log", __logging_timeinfo)) { \
     strcpy(__logging_file_name,"hare.log"); \
   } \
-  FILE *__logging_file = fopen(__logging_file_name,"wt"); \
+  __logging_file = fopen(__logging_file_name,"wt"); \
   if (NULL == __logging_file) { \
-    fprintf(stderr, "ERROR! Logging file can't be open!"); \
+    fprintf(stderr, "[  ERROR  ] Logging file can't be open!"); \
     __logging = 0; \
   }
 
-#define WRITE(str) \
-  printf("`"__FILE__"`:%d: %s\n", __LINE__, str);
+#define __OUT(str) \
+  if (__logging) \
+    fprintf(__logging_file, str);
+
+#define WRITE(pre, str) \
+  if (__logging) { \
+    sprintf(__logging_buf, "%s `"__FILE__"`:%d: %s\n", pre, __LINE__, str); \
+    __OUT(__logging_buf); \
+  }
+
+#define INFO(str) \
+  WRITE(" [  INFO  ]", str); \
+
+#define MEMORY(str) \
+  WRITE("[  MEMORY ]", str) \
+
+#define IO(str) \
+  WRITE(" [   IO   ]", str)
+
+#define ERROR(str) \
+  WRITE("[  ERROR  ]", str)
+
+#define WARNING(str) \
+  WRITE("[ WARNING ]", str)
+
 
 #define DEINIT_LOGGING \
   if (__logging) { \
@@ -28,7 +53,7 @@
 
 GLOBAL_LOGGING
 
-int test_func();
+int test_func(const char *lol);
 
 int main(int argc, char *argv[], char *env[])
 {
@@ -42,14 +67,21 @@ int main(int argc, char *argv[], char *env[])
   strftime(s, 80, "%d%m%Y_%H%M%S", timeinfo);
   printf("%s \n", s);
 
-  test_func();
+  test_func("Blah Blah Blah, Mr. Freeman");
 
   DEINIT_LOGGING
   return 0;
 }
 
 
-int test_func() {
-  WRITE("GET THE FUCKING FLOWERS!!!!!")
+int test_func(const char * lol) {
+  char str[1000] = "";
+  sprintf(str, "I fucked the bitch: '%s'",lol);
+  INFO(str);
+  MEMORY(str);
+  IO(str);
+  ERROR(str);
+  WARNING(str);
+
   return 0;
 }
