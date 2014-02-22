3a250e3adee64696c2ceee5fd203b3717faf0ae4
Korovin Viktor <sarvash.inc@gmail.com>
2014-02-22 22:21:00 +0700

    + Работает в функциях из других файлов + Не компилится QtCreater-ом при незадефайненом DEBUG, при этом стандартным gcc всё компилится нормально

--- src/hare.h
+++ src/hare.h
@@ -1,16 +1,9 @@
 #ifndef HARE_H
 #define HARE_H
+#include "logging.h"
 #include <inttypes.h>
 #include <stdint.h>
 
-typedef struct {
-  int argc;
-  char *argv[];
-  char *env[];
-  int8_t keys;
-  /* 0x80,0x40, ... , 0x2, 0x1: Ø, Ø, Ø, Add, eXtract, Delete, List, inTegrity check */
-  char *archName;
-  char *workFiles[];
-} Context;
+int test_func(const char *lol);
 
 #endif // HARE_H
--- src/main.c
+++ src/main.c
@@ -1,15 +1,13 @@
 #include "logging.h"
+#include "hare.h"
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
-
 
 GLOBAL_LOGGING
 
 #define TEST(str, ...) \
   printf(str, ##__VA_ARGS__);
-
-int test_func(const char *lol);
 
 int main(int argc, char *argv[], char *env[])
 {
@@ -31,16 +29,5 @@ int main(int argc, char *argv[], char *env[])
   TEST("%s", "blah");
 
   printf("Op\n");
-  return 0;
-}
-
-
-int test_func(const char * lol) {
-  INFO("Opana %s", "__Ne ponyal na");
-  MEMORY("Lol is: %s", lol);
-  IO("IO-shechki bibib %d %d",12, 13);
-  ERROR("A ya eby gusei");
-  WARNING("op %s %s %s","musorok","!","Ne shei mne srok");
-
   return 0;
 }
