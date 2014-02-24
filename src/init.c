0f5b83fc09843267f29334f470e8ebcc6f63a672
Alex <acamukhin@gmail.com>
2014-02-24 22:40:51 +0700

    Изменения коренные + ParseArgs теперь находится отдельно от main() + Коррекция в hare.h(переопределил функцию parseArgs()) - Была сделана попытка подключить logging.h, но есть проблема: когда подключаешь один исходный файл, то все норм, а когда несколько -- FATAL ERROR

---
+++ src/init.c
@@ -0,0 +1,47 @@
+#include "init.h"
+
+Context* parseArgs(int argc, char **argv, char **evn){
+
+    char *str;
+    char id;
+    int i; int numFiles=0;
+    FileNode *head = NULL;
+    FileNode *iter;
+    FILE *f = NULL;
+    Context *cnt = (Context *)malloc(sizeof(Context));
+    cnt->workFiles = (char **)malloc(sizeof(char *));
+    if (argc == 1) {
+      return -1;
+    }
+    str = argv[1];
+    if (*(str) == '-') {
+      id = *(str+1);
+      switch (id) {
+          case 'a':
+          if (argc < 4) break;
+              for (i=3;i<argc;i++){
+                  f = fopen(argv[i], "r");
+                  if (f != NULL){
+                      if (head == NULL){
+                          head       = (FileNode*)malloc(sizeof(FileNode));
+                          iter       = head;
+                      } else {
+                          iter->next = (FileNode*)malloc(sizeof(FileNode));
+                          iter       = iter->next;
+                      }
+                      *(cnt->workFiles + numFiles) = argv[i];
+                      numFiles++;
+                      cnt->workFiles = (char **)realloc(cnt->workFiles, (numFiles + 1)*sizeof(char *));
+                      iter->file     = f;
+                      iter->next     = NULL;
+                      iter->info     = (FileInfo *)malloc(sizeof(FileInfo));
+                  }
+              }
+              break;
+          default:
+              break;
+          }
+      *(cnt->workFiles + numFiles) = (char *)calloc(1, sizeof(char));//Как бы нулевой символ
+      }
+    return cnt;
+}
