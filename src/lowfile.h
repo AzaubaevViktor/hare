3a22812a0a0b33c1ef71614fe8bc811bcf112b9b
Korovin Viktor <sarvash.inc@gmail.com>
2014-03-16 00:31:00 +0700

    readInt64, readChar ! почищен код ! избавился от магических чисел в пользу define

--- src/lowfile.h
+++ src/lowfile.h
@@ -16,6 +16,7 @@
 
 #define BUF_LEN (64)
 #define BUF_LEN_BITS (BUF_LEN*8)
+#define INT64SIZE (sizeof(int64_t))
 
 #define IO_WRITE_ERROR (2451)
 #define IO_READ_ERROR  (2452)
@@ -26,5 +27,7 @@ int writeInt64(FILE *f, int64_t num, int drop);
 int writeChar(FILE *f, char ch, int drop);
 
 int readNBytes(FILE *f, uint64_t N, char *str, size_t *read_bytes);
+int readInt64(FILE *f, int64_t *num, size_t *read_bytes);
+int readChar(FILE *f, char ch, size_t *read_bytes);
 
 #endif // LOWFILE_H
