1ada2ec17597af0664c2b33b072a2a343546b4c5
Alex Mukhin <acamukhin@gmail.com>
2014-05-22 22:13:18 +0700

    Мелкие исправления

--- src/check.c
+++ src/check.c
@@ -4,20 +4,20 @@ int checkIntegrity_my(FILE *arch){
     ArchFileInfo *info = (ArchFileInfo *)malloc(sizeof(ArchFileInfo));
     info->fileInfo = (FileInfo *)malloc(sizeof(FileInfo));
     crc realsum, shouldsum;
-    int i = 0;
     initRdCrc();
     int err = 0;
 
     while ((err = checkingHeader(arch, info) != IO_EOF)){
         if (isFolder(info->fileInfo->name)) continue;
+        printf("%s\n", info->fileInfo->name);
         printf("Header: ");
-        if (err == 0) printf("OK %d\n", i);
-        else if (err == 1) printf("BAD %d %lx\n", i,  info->HeaderCheckSum);
+        if (err == 0) printf("OK\n");
+        else if (err == 1) printf("BAD %lx\n",  info->HeaderCheckSum);
         err = checkingData(arch, info, &realsum, &shouldsum);
         printf("Data  : ");
-        if (err == 0) printf("OK %d\n", i); else
-            if (err == 1) printf("BAD %d %lx %lx\n", i, realsum, shouldsum);
-        i++;
+        if (err == 0) printf("OK\n"); else
+            if (err == 1) printf("BAD %lx %lx\n", realsum, shouldsum);
+
     }
     free(info->fileInfo);
     free(info);
@@ -25,12 +25,13 @@ int checkIntegrity_my(FILE *arch){
 }
 int checkingHeader(FILE *arch, ArchFileInfo *info){
     initRdCrc();
-
-
     int err = readHeader(arch, info);
     if (feof(arch)) return IO_EOF;
     if (err == SIGNATURE_ERROR){
+        printf("Check Error on %lx\n", ftell(arch));
         findSignature(arch);
+        if (feof(arch)) return IO_EOF;
+        printf("Find on %lx\n", ftell(arch));
         return checkingHeader(arch, info);
     }
     if (err == HASH_HEADER_CHECK_ERROR) return 1; else return 0;
@@ -38,10 +39,8 @@ int checkingHeader(FILE *arch, ArchFileInfo *info){
 int checkingData(FILE *arch, ArchFileInfo *info, crc *real, crc *should){
     initRdCrc();
     size_t read_bytes;
-    //int i; char ch;
     char *str = (char *)malloc(info->dataSize);
     readNBytes(arch, info->dataSize, str, &read_bytes);
-    //for (i = 0; i < info->dataSize;i++) readChar(arch, &ch, &read_bytes);
     *real = getRdCrc();
     readCrc(arch, should, &read_bytes);
     free(str);
@@ -50,5 +49,4 @@ int checkingData(FILE *arch, ArchFileInfo *info, crc *real, crc *should){
     } else {
         return 1;
     }
-
 }
